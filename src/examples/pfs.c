a6629a29311ec0306284370179f3f1c7bc671235
klaar36 <klas.arvidsson@liu.se>
2016-05-04 14:45:21 +0200

    increase interrupt frequency in tests

--- src/examples/pfs.c
+++ src/examples/pfs.c
@@ -1,6 +1,6 @@
 /* klaar@ida (cleanup of previous code by various IDA-employees)
    
-   pintos -v -k -T 120 --fs-disk=2 --qemu -p ../examples/pfs -a pfs -p ../examples/pfs_writer -a pfs_writer -p ../examples/pfs_reader -a pfs_reader -g messages -- -f -q run pfs
+   pintos -v -k -T 120 --fs-disk=2 --qemu -p ../examples/pfs -a pfs -p ../examples/pfs_writer -a pfs_writer -p ../examples/pfs_reader -a pfs_reader -g messages -- -F=20000 -f -q run pfs
    
    Tests that read and write are properly synchronized. Also stresses
    the filesystem somewhat.
